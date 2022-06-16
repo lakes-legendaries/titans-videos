c21de265ca4b82892852725d640ac0e3769d4a46
Mike Powell <mike@lakeslegendaries.com>
2022-06-16 12:36:34 -0400

    refactoring to create multiple video file formats

--- make_clips.py
+++ make_clips.py
@@ -1,7 +1,10 @@
 """Make video clips (former gifs)"""
 
+from __future__ import annotations
+
 import os
 from subprocess import run
+from time import sleep
 
 
 # set working directory
@@ -11,36 +14,37 @@ os.chdir(upload_dir)
 # parameters for clip extraction
 params = {
     'elements': {
-        'source': 'landing_video.mp4',
+        'source': 'landing_video.h264.mp4',
         'start': '00:00:15.75',
         'duration': '00:00:05.00',
     },
     'interactive': {
-        'source': 'landing_video.mp4',
+        'source': 'landing_video.h264.mp4',
         'start': '00:00:22.00',
         'duration': '00:00:15.50',
     },
     'nowait': {
-        'source': 'landing_video.mp4',
+        'source': 'landing_video.h264.mp4',
         'start': '00:00:38.00',
         'duration': '00:00:11.00',
     },
     'constructed': {
-        'source': 'landing_video.mp4',
+        'source': 'landing_video.h264.mp4',
         'start': '00:01:04.50',
         'duration': '00:00:09.00',
     },
     'empire': {
-        'source': 'empire_video.mp4',
+        'source': 'empire_video.h264.mp4',
         'start': '00:01:11.50',
         'duration': '00:00:19.50',
         'speedup': 2,
     },
 }
 
-# perform extraction
-for ofname, controls in params.items():
-    tmp_fname = f"{ofname}-tmp.mp4"
+
+def crop(name: str, controls: dict[str, str]) -> str:
+    """Crop video, return output filename"""
+    ofname = f'{name}-cropped.mp4'
     run(
         [
             'ffmpeg',
@@ -54,39 +58,136 @@ for ofname, controls in params.items():
             '-an',
             '-filter:v',
             'crop=1920:1000:0:0',
-            tmp_fname,
+            ofname,
         ],
         capture_output=True,
         check=True,
     )
-    if 'speedup' in controls:
-        tmp_fname2 = 'speedup' + tmp_fname
-        run(
-            [
-                'ffmpeg',
-                '-i',
-                tmp_fname,
-                '-y',
-                '-filter:v',
-                f"setpts=PTS/{controls['speedup']}",
-                tmp_fname2,
-            ],
-            capture_output=True,
-            check=True,
-        )
-        os.remove(tmp_fname)
-        tmp_fname = tmp_fname2
+    return ofname
+
+
+def speedup(ifname: str, speedup: int) -> str:
+    """Speedup video, return output filename"""
+    ofname = f'{ifname}-speedup.mp4'
     run(
         [
             'ffmpeg',
             '-i',
-            tmp_fname,
+            ifname,
             '-y',
-            '-vf',
-            'tpad=stop_mode=clone:stop_duration=2',
-            f'{ofname}_clip.mp4',
+            '-filter:v',
+            f"setpts=PTS/{speedup}",
+            ofname,
         ],
         capture_output=True,
         check=True,
     )
-    os.remove(tmp_fname)
+    return ofname
+
+
+def add_pause(ifname, ofname):
+    """add pause to end of video"""
+    run(
+        [
+            'ffmpeg',
+            '-i',
+            ifname,
+            '-y',
+            '-vf',
+            'tpad=stop_mode=clone:stop_duration=2',
+            ofname,
+        ],
+        capture_output=True,
+        check=True,
+    )
+
+
+def convert(ifname):
+    """convert to other video formats"""
+    run(
+        [
+            'ffmpeg',
+            '-y',
+            '-i',
+            ifname,
+            '-c:v',
+            'libx265',
+            '-vtag',
+            'hvc1',
+            ifname.replace('-h264', '-h265'),
+        ],
+        capture_output=True,
+        check=True,
+    )
+    run(
+        [
+            'ffmpeg',
+            '-y',
+            '-i',
+            ifname,
+            '-c:v',
+            'libvpx-vp9',
+            '-b:v',
+            '0',
+            '-crf',
+            '50',
+            '-pass',
+            '1',
+            '-an',
+            '-f',
+            'null',
+            '/dev/null',
+        ],
+        capture_output=True,
+        check=True,
+    )
+    run(
+        [
+            'ffmpeg',
+            '-y',
+            '-i',
+            ifname,
+            '-c:v',
+            'libvpx-vp9',
+            '-b:v',
+            '0',
+            '-crf',
+            '50',
+            '-pass',
+            '2',
+            '-c:a',
+            'libopus',
+            ifname.replace('-h264.mp4', '.webm'),
+        ],
+        capture_output=True,
+        check=True,
+    )
+
+
+# perform extraction
+if __name__ == '__main__':
+    for name, controls in params.items():
+        
+        # set counters and names
+        tmp_fname = []
+        h264 = f'{name}_clip-h264.mp4'
+
+        # create videos
+        tmp_fname.append(crop(name, controls))
+        if 'speedup' in controls:
+            tmp_fname.append(
+                speedup(tmp_fname[-1], controls['speedup'])
+            )
+        add_pause(tmp_fname[-1], h264)
+        convert(h264)
+        
+        # rename files (had to do a workaround for ffmpeg padding)
+        h265 = h264.replace('-h264.mp4', '-h265.mp4')
+        webm = h264.replace('-h264.mp4', '.webm')
+        os.rename(h264, h264.replace('-', '.'))
+        os.rename(h265, h265.replace('-', '.'))
+        os.rename(webm, webm.replace('-', '.'))
+
+        # clean up
+        while tmp_fname:
+            os.remove(tmp_fname.pop())
