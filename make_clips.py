bf9865d21616f65591d7013186bf1214d4dd989b
Mike Powell <mike@lakeslegendaries.com>
2022-06-17 15:20:28 -0400

    adding av1 format, creating convert bash script

--- make_clips.py
+++ make_clips.py
@@ -102,92 +102,35 @@ def add_pause(ifname, ofname):
     )
 
 
-def convert(ifname):
-    """convert to other video formats"""
-    run(
-        [
-            'ffmpeg',
-            '-y',
-            '-i',
-            ifname,
-            '-c:v',
-            'libx265',
-            '-vtag',
-            'hvc1',
-            ifname.replace('-h264', '-h265'),
-        ],
-        capture_output=True,
-        check=True,
-    )
-    run(
-        [
-            'ffmpeg',
-            '-y',
-            '-i',
-            ifname,
-            '-c:v',
-            'libvpx-vp9',
-            '-b:v',
-            '0',
-            '-crf',
-            '50',
-            '-pass',
-            '1',
-            '-an',
-            '-f',
-            'null',
-            '/dev/null',
-        ],
-        capture_output=True,
-        check=True,
-    )
-    run(
-        [
-            'ffmpeg',
-            '-y',
-            '-i',
-            ifname,
-            '-c:v',
-            'libvpx-vp9',
-            '-b:v',
-            '0',
-            '-crf',
-            '50',
-            '-pass',
-            '2',
-            '-c:a',
-            'libopus',
-            ifname.replace('-h264.mp4', '.webm'),
-        ],
-        capture_output=True,
-        check=True,
-    )
-
-
 # perform extraction
 if __name__ == '__main__':
     for name, controls in params.items():
-        
-        # set counters and names
+
+        # try loop to clean up temporary files
         tmp_fname = []
-        h264 = f'{name}_clip-h264.mp4'
+        try:
 
-        # create videos
-        tmp_fname.append(crop(name, controls))
-        if 'speedup' in controls:
-            tmp_fname.append(
-                speedup(tmp_fname[-1], controls['speedup'])
+            # create videos
+            tmp_fname.append(crop(name, controls))
+            if 'speedup' in controls:
+                tmp_fname.append(
+                    speedup(tmp_fname[-1], controls['speedup'])
+                )
+            add_pause(tmp_fname[-1], f'{name}_clip.mp4')
+
+            # convert formats
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            run(
+                [
+                    dir_path + '/' + 'convert',
+                    tmp_fname[-1],
+                    upload_dir,
+                ],
+                capture_output=True,
+                check=True,
             )
-        add_pause(tmp_fname[-1], h264)
-        convert(h264)
-        
-        # rename files (had to do a workaround for ffmpeg padding)
-        h265 = h264.replace('-h264.mp4', '-h265.mp4')
-        webm = h264.replace('-h264.mp4', '.webm')
-        os.rename(h264, h264.replace('-', '.'))
-        os.rename(h265, h265.replace('-', '.'))
-        os.rename(webm, webm.replace('-', '.'))
 
-        # clean up
-        while tmp_fname:
-            os.remove(tmp_fname.pop())
+        # clean up temporary files
+        finally:
+            while tmp_fname:
+                os.remove(tmp_fname.pop())
